12d21a124e8e41a9b445017d670b51f3df7f1b1f
Yash Prakash <yashprakash13@gmail.com>
2022-09-29 19:50:56 -0400

    added simple rest api init project.

---
+++ restapi_crud/README.md
@@ -0,0 +1,10 @@
+# Simple REST API
+
+## Run the api
+
+```go
+go run main.go
+```
+
+Navigate to the `localhost:10000` page to see the homepage of the API!
+
---
+++ restapi_crud/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to the Home Page!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func main() {
+	handleRequests()
+}
